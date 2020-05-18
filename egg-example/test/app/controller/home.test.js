6de5958e2627d785c25e207a93df2c6e98cdcf15
wu <18242086605@163.com>
2020-05-18 10:55:01 +0800

    demoSigned-off-by: wu <18242086605@163.com>

--- egg-example/test/app/controller/home.test.js
+++ egg-example/test/app/controller/home.test.js
@@ -14,7 +14,7 @@ describe('test/app/controller/home.test.js', () => {
   it('should GET /', () => {
     return app.httpRequest()
       .get('/')
-      .expect('hi22, egg')
+      .expect('hi, egg')
       .expect(200);
   });
 });
